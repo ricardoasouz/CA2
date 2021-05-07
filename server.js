732752fb14b1ec943e87c7d2e0f7071e8a482bcc
ricardoasouz <ricardoasouz@hotmail.com>
2021-05-07 15:34:05 +0100

    heroku

--- server.js
+++ server.js
@@ -33,3 +33,19 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://iwaca2.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
